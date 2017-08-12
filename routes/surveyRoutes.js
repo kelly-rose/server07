37e36abf90ef213459cfacd5ce7f3b78b81b4ee0
kelly <kellyrose.evergreen@gmail.com>
2017-08-13 00:13:17 +0900

    5th commit : 177, 178

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,39 +1,61 @@
-const mongoose  = require('mongoose');
+//section 12 - 177
+const _ = require('lodash');
+const Path = require('path-parser');
+const {URL} = require('url');
+
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
-const requireCredits= require('../middlewares/requireCredits');
+const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 //theory 10 - 125,126
-module.exports = app =>{
+module.exports = app => {
 
     //section 10 - 138
-    app.get('/api/surveys/thanks',(req,res)=>{
+    app.get('/api/surveys/thanks', (req, res) => {
         res.send('Thanks for voting!');
     });
 
     /* https://tkfkdgodyzofflfhwm.localtunnel.me/api/surveys/webhooks */
-    app.post('/api/surveys/webhooks',(req,res)=>{
-       console.log(req.body);
-       res.send({});
+    app.post('/api/surveys/webhooks', (req, res) => {
+        console.log(req.body);
+
+        //section 12 - 177
+        const events = _.map(req.body, ({email,url}) => {
+            const pathname = new URL(url).pathname;
+            console.log(pathname); ///api/surveys/598f0750d1f94004609c275e/yes
+            const p = new Path('/api/surveys/:surveyId/:choice');
+            console.log(p);
+            const match = p.test(pathname);
+            console.log(match); //{ surveyId: '598f07e690798a2e7cc09a16', choice: 'no' }
+            //section 12 - 178
+            if (match) {
+                return {email: email, surveyId: match.surveyId, choice: match.choice};
+            }
+        });
+
+        console.log(events);
     });
 
 
-    app.post('/api/surveys',requireLogin,requireCredits, async (req,res)=>{
+    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const {title, subject, body, recipients} = req.body;
 
         const survey = new Survey({
             title,
             subject,
             body,
-            recipients : recipients.split(',').map(email => { return {email:email.trim()}}),
-            _user : req.user.id,
-            dateSent : Date.now()
+            recipients: recipients.split(',').map(email => {
+                return {email: email.trim()}
+            }),
+            _user: req.user.id,
+            dateSent: Date.now()
         });
 
         //Great place to send an email !
-        const mailer = new Mailer(survey,surveyTemplate(survey));
+        const mailer = new Mailer(survey, surveyTemplate(survey));
         try {
             await mailer.send();  //section 10 - 134
 
@@ -42,7 +64,7 @@ module.exports = app =>{
             req.user.credits -= 1;
             const user = await req.user.save(); //save DB with updating user
             res.send(user);
-        }catch (err){
+        } catch (err) {
             res.status(422).send(err);
         }
 
